52e0a1389ae9da95c96722b3b7103d91bfc831f6
caramirezal <caramirezal@ciencias.unam.mx>
2020-03-25 14:21:04 +0100

    loading sparse matrices to seurat

---
+++ reports/1_seurat.R
@@ -0,0 +1,9 @@
+## dependencies
+library(Seurat)
+library(miceadds)   ## for load.Rdata 
+
+metadata <- read.table('')
+
+SRA553822_SRS2119548 <- load('~/sc/PanglaoDB/var/www/html/SRA/SRA.final/SRA553822_SRS2119548.sparse.RData')
+
+class(SRA553822_SRS2119548)
