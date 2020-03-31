52d854fedd2a40fd2f51db937c37458cb075ae9e
caramirezal <cramireza@ciencias.unam.mx>
2020-03-31 21:39:43 +0200

    download landscape data

--- src/2_download_data.sh
+++ src/2_download_data.sh
@@ -2,3 +2,8 @@
 mkdir data/panglaodb
 cd data/panglaodb
 git clone https://github.com/oscar-franzen/PanglaoDB.git
+
+## Downloading landscape data
+## the file is in zip format
+wget https://ndownloader.figshare.com/articles/7235471?private_link=2892d1cf91752e08a963 \
+     -P data/landscape_data/
