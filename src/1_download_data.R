3d9a93995322d91802c9ab3185df5b9ed3f35c66
caramirezal <cramireza@ciencias.unam.mx>
2020-04-01 01:50:05 +0200

    adding stewart download

--- src/1_download_data.R
+++ src/1_download_data.R
@@ -1,17 +1,30 @@
 ## Downloading data
-path2project <- '~/sc/sars-cov2'
+path2project <- '/media/ag-cherrmann/cramirez/sars-cov2/'
 
-if ( ! dir.exists('data/kim2015/')) {
-        dir.create('data/kim2015/')
+kim_dir <- paste0(path2project, 'data/kim2015/')
+if ( ! dir.exists(kim_dir) ) {
+        dir.create(kim_dir)
         url <- 'https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE69405&format=file&file=GSE69405%5FPROCESSED%5FGENE%5FTPM%5FALL%2Etxt%2Egz'
-        download.file(url, destfile = 'data/kim2015/GSE69405_PROCESSED_GENE_TPM_ALL.txt.gz')
+        download.file(url, 
+                      destfile = paste0(kim_dir, 'GSE69405_PROCESSED_GENE_TPM_ALL.txt.gz') )
 }
 
-if ( ! dir.exists('data/stewart2020/')) {
-        dir.create('data/stewart2020/')
+stewart_dir <- paste0(path2project, 'data/stewart2020/')
+if ( ! dir.exists(stewart_dir) ) {
+        dir.create(stewart_dir)
         url <- 'https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE138474&format=file'
-        download.file(url, destfile = 'data/stewart2020/GSE138474_RAW.tar')
-        untar('data/stewart2020/GSE138474_RAW.tar', exdir = 'data/stewart2020/')
-        untar('data/stewart2020/GSM4104144_SC16.LB17028.matrix.tar.gz', 
-              exdir = 'data/stewart2020/')
+        download.file(url, 
+                      destfile = paste0(stewart_dir, 'GSE138474_RAW.tar'))
+        untar(paste0(stewart_dir, 'GSE138474_RAW.tar'), 
+              exdir = stewart_dir)
 }
+
+## Downloading landscape data
+#landscape_data_dir <- paste0(path2project, 'data/landscape_data/')
+#if ( ! dir.exists(landscape_data_dir) ) {
+#        dir.create(landscape_data_dir)
+#        url <- 'https://ndownloader.figshare.com/articles/7235471?private_link=2892d1cf91752e08a963'
+#        download.file(url, destfile = paste0(landscape_data_dir, 'landscape_data'))
+#}
+
+
