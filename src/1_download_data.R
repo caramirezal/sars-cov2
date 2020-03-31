32c81a771a523cd49b0e7b51ab7c4156ca4aa9d9
caramirezal <caramirezal@ciencias.unam.mx>
2020-03-31 21:08:56 +0200

    download lung cancer datasets and vis

--- src/1_download_data.R
+++ src/1_download_data.R
@@ -1,4 +1,5 @@
 ## Downloading data
+path2project <- '~/sc/sars-cov2'
 
 if ( ! dir.exists('data/kim2015/')) {
         dir.create('data/kim2015/')
@@ -6,3 +7,11 @@ if ( ! dir.exists('data/kim2015/')) {
         download.file(url, destfile = 'data/kim2015/GSE69405_PROCESSED_GENE_TPM_ALL.txt.gz')
 }
 
+if ( ! dir.exists('data/stewart2020/')) {
+        dir.create('data/stewart2020/')
+        url <- 'https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE138474&format=file'
+        download.file(url, destfile = 'data/stewart2020/GSE138474_RAW.tar')
+        untar('data/stewart2020/GSE138474_RAW.tar', exdir = 'data/stewart2020/')
+        untar('data/stewart2020/GSM4104144_SC16.LB17028.matrix.tar.gz', 
+              exdir = 'data/stewart2020/')
+}
